18c5c733f913091afc102763c8a73fca1051db29
macooleryo <macooleryo@gmail.com>
2026-05-09 13:58:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@
 */
 
 const settings = {
-  packname: 'Adevos-X Tech',
-  author: 'Adevos',
-  botName: "𝐀𝐝𝐞𝐯𝐨𝐬-𝐗 𝐁𝐨𝐭",
-  botOwner: 'adevos', // Your name
-  ownerNumber: process.env.OWNER_NUMBER || '255675421210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Anonymous Ai V2',
+  author: 'Zuka',
+  botName: "𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬 𝐀𝐢 𝐕2",
+  botOwner: '𝐙𝐮𝐤𝐚', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '263787786042', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
@@ -18,7 +18,7 @@ const settings = {
   version: "2.0.0",
   updateBranch: 'main',
   updateZipToken: '',
-  updateZipUrl: "https://github.com/adevosxtech/adevosX-Bot/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/macooleryo/adevosX-Bot/archive/refs/heads/main.zip",
   timezone: 'Africa/nairobi',
 };
 
